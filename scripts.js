39a5b8d6e085e6c00f2a56c45be3ab03083a20da
Obez99 <giuliogx@live.it>
2021-10-27 15:18:00 +0200

    Converts user input into an array

--- scripts.js
+++ scripts.js
@@ -1,8 +1,24 @@
 //Salvo l'array casuale in una variabile globale per poterlo confrontare con l'input dell'utente
 const randomArray = generateRandomArray();
-
+let numbers;
 //Faccio vedere i numeri all'utente tramite un alert
 alert(randomArray);
+
+//Avvia un timer di trenta secondi
+//Allo scadere del tempo chiedere all'utente di inserire dei numeri con un prompt()
+setTimeout(function () {
+  numbers = prompt("Inserisci cinque numeri separati da spazi o virgole")
+
+  //Converte i numeri in un array
+  let userArray = [];
+  if (numbers.includes(" "))
+    userArray = numbers.split(" ")
+  else
+    userArray = numbers.split(",")
+  console.log(userArray);
+
+  //Controllare quali e quanti numeri sono stati individuati
+}, 0)
 
 
 
