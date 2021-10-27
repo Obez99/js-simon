7ae09b4cfe9d5c0a5eea8e11dfdf163ed5948465
Obez99 <giuliogx@live.it>
2021-10-27 14:04:48 +0200

    Displays random numbers to user

--- scripts.js
+++ scripts.js
@@ -1,3 +1,15 @@
+//Salvo l'array casuale in una variabile globale per poterlo confrontare con l'input dell'utente
+const randomArray = generateRandomArray();
+
+//Faccio vedere i numeri all'utente tramite un alert
+alert(randomArray);
+
+
+
+
+
+
+
 //Creo una funzione che ritorna un array di 5 numeri casuali
 function generateRandomArray() {
   const arr = [];
